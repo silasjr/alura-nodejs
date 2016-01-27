512e354225c415288003dd2cbc653f6644759a3c
silas <jose.silasjr.bb@gmail.com>
2016-01-27 16:27:48 -0400

    arquivos da aula 2

--- app.js
+++ app.js
@@ -1,5 +1,4 @@
 var app = require('./config/express')();
-var rotaProdutos = require('./app/routes/produtos')(app);
 
 app.listen(3000, function(){
     console.log("Servidor rodando");
--- config/express.js
+++ config/express.js
@@ -1,8 +1,19 @@
 var express = require('express');
-var app = express();
-app.set('view engine', 'ejs');
-app.set('views', './app/views');
+var load = require('express-load');
+
 
 module.exports = function(argument) {
+	
+	var app = express();
+
+	app.set('view engine', 'ejs');
+	app.set('views', './app/views');
+
+	// a ordem de carregamento influencia
+	// ja carrega o modulo e invoca o objeto
+	load('routes', {cwd: 'app'})
+		.then('infra')
+	.into(app);
+
 	return app;
 }
