b7440ee4c94333b82604bc80523b96a4f829b8ac
silas <jose.silasjr.bb@gmail.com>
2016-01-27 17:01:10 -0400

    arquivos da aula 2

--- app/routes/produtos.js
+++ app/routes/produtos.js
@@ -2,7 +2,7 @@ module.exports = function(app) {
 	app.get('/produtos', function(req, res){
 		var connection = app.infra.connectionFactory();
 		// cria um novo contexto de uso 'this'
-		var produtoBanco = new app.infra.produtosDAO(connection);
+		var produtoBanco = new app.infra.ProdutosDAO(connection);
 
 		produtoBanco.lista(function(err, results){
 			res.render("produtos/lista", {lista: results});
