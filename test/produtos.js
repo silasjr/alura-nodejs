e5a26ec5e931a3b9350bf18e550b67fb527a4130
Silas Jr <silas@silas>
2016-02-08 19:40:21 -0400

    termino dos exercicios do capitulo 7

--- test/produtos.js
+++ test/produtos.js
@@ -1,6 +1,15 @@
 var express = require('../config/express')();
 var request = require('supertest')(express);
 describe('#ProdutosController', function(){
+	//TODO pesquisar sobre node-databse-cleaner
+	beforeEach(function(done){
+		var conn = express.infra.connectionFactory();
+		conn.query('delete from livros', function(ex, result){
+			if (!ex) {
+				done();
+			}	
+		});
+	});
 
 	it('#listagem json', function(done){
 		request.get('/produtos')
